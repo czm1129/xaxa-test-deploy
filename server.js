6c28349fc071c061eeab0429d3acfd3a8216f15f
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2014-12-29 15:24:17 +0300

    version 3

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<html><head><meta charset="utf-8"/></head><body>Version 2.0</body></html>');
+    res.end('<html><head><meta charset="utf-8"/></head><body>Version 3.0</body></html>');
 }).listen(5000);
