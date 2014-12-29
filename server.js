12405ab783f528dd5439a8e47f5b6e416232e9a5
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2014-12-29 15:40:41 +0300

    version 4 and everDeploy.sh

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<html><head><meta charset="utf-8"/></head><body>Version 3.0</body></html>');
+    res.end('<html><head><meta charset="utf-8"/></head><body>Version 4.0</body></html>');
 }).listen(process.env.PORT || 5000);
