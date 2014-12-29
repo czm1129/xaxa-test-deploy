e4a91cdb5d23f95f880f15dd784f173712c6b86e
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2014-12-29 15:10:16 +0300

    initial version

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<html><head><meta charset="utf-8"/></head><body>Version 1.0</body></html>');
+}).listen(5000);
