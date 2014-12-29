17da84c0b2c6bdc33c3fd3764869b6028f476d28
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2014-12-29 15:29:10 +0300

    substitute port number for Heroku

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<html><head><meta charset="utf-8"/></head><body>Version 3.0</body></html>');
-}).listen(5000);
+}).listen(process.env.PORT || 5000);
